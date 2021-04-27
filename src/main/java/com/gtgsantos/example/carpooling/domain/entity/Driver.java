0a59512a3326d6184a36e9357952f5fab5b2c3d0
Guilherme Santos <guilherme.santos@gmail.com>
2021-04-26 23:58:10 -0300

    new libraries and entities

---
+++ src/main/java/com/gtgsantos/example/carpooling/domain/entity/Driver.java
@@ -0,0 +1,28 @@
+package com.gtgsantos.example.carpooling.domain.entity;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+
+@Data
+@Entity
+@Schema(description = "Represents a drive inside platform")
+public class Driver {
+
+    @Id
+    @GeneratedValue
+    Long id;
+
+    @Schema(description = "Driver's name")
+    @Size(min=2, max=255)
+    String name;
+
+    @Schema(description = "Driver's  Birthday")
+    Date birthDate;
+}
